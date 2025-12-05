29126cdbd0d67a9eb18e22c970c28e2b830f4b62
Nate <devDelay@gmail.com>
2025-12-05 16:23:55 -0500

    updates

--- enforce.sh
+++ enforce.sh
@@ -32,10 +32,16 @@ print_info "Enforcing DISA STIG V2R6 compliance on CentOS 10 Stream systems..."
 print_warning "This will make significant security changes to your systems!"
 print_warning "Ensure you have backups and have tested in a non-production environment."
 
+# Set XML report path (relative to current directory)
+XML_REPORT_PATH="${PWD}/stig-compliance-report.xml"
+export XML_PATH="${XML_REPORT_PATH}"
+
+print_info "XML compliance report will be saved to: ${XML_REPORT_PATH}"
+
 # Run the playbook
 ansible-playbook -v -b -i inventory.yml playbook.yml --ask-become-pass
 
 print_info "STIG enforcement completed!"
-print_info "Check the XML report for compliance results."
+print_info "XML compliance report saved to: ${XML_REPORT_PATH}"
 print_warning "Reboot may be required. Review changes before rebooting."
 
